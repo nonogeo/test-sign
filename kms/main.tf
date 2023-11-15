6b1ca85de7116775722d67c6eef0d861a8f0eaeb
nonogeo <72592778+nonogeo@users.noreply.github.com>
2023-11-15 23:38:12 +0200

    Updated kms module so it creates a HSM key

--- kms/main.tf
+++ kms/main.tf
@@ -1,16 +1,17 @@
 resource "google_kms_key_ring" "keyring" {
   name     = "keyring-test"
   location = "europe-north1"
-  project = var.project_id
+  project  = var.project_id
 }
 
-resource "google_kms_crypto_key" "example-asymmetric-sign-key" {
-  name     = "crypto-asymetric-key-example"
+resource "google_kms_crypto_key" "example-asymmetric-sign-key-euno1" {
+  name     = "crypto-asymetric-key-example-hsm"
   key_ring = google_kms_key_ring.keyring.id
   purpose  = "ASYMMETRIC_SIGN"
 
   version_template {
-    algorithm = "EC_SIGN_P384_SHA384"
+    algorithm        = "EC_SIGN_P384_SHA384"
+    protection_level = "HSM"
   }
 
   lifecycle {
